f68046048821245532a0c232e9e12d95889d3f2c
Noah A Klem <noahaklem@gmail.com>
2021-11-18 18:29:47 -0700

    commit tree 5cdfeb90b8b0531e7b2753103129d351850e4394

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
+import React from "react"
+
 // Code EyesOnMe Component Here
+export default class EyesOnMe extends React.Component {
+  focus = (e) => {
+    console.log("Good!")
+  } 
+
+  blur = (e) => {
+    console.log("Hey! Eyes on me!")
+  }
+
+  render() {
+    return <div>
+      <button type="submit" value = "submit" onFocus={this.focus} onBlur={this.blur}> Eyes on me
+      </button>
+    </div>
+  }
+} 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import React from "react";
+
+export default class Keypad extends React.Component {
+  keyUp = (event) => {
+    console.log("Entering password...")
+  }
+  render() {
+    return <input onKeyUp={this.keyUp} type = "password" ></input>
+  }
+}
